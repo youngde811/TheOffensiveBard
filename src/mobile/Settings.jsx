c123d83b19bc18e0105c9f7ec9b9bf20873c5b8e
David Young <sporty-iron883@protonmail.com>
2025-11-20 13:39:04 -0500

    Decrease log noise

--- src/mobile/Settings.jsx
+++ src/mobile/Settings.jsx
@@ -488,7 +488,7 @@ export default function Settings({ appConfig, setDismiss }) {
 
             <View style={styles.aboutRow}>
               <Text style={[styles.aboutLabel, { color: colors.textMuted }]}>Version</Text>
-              <Text style={[styles.aboutValue, { color: colors.text }]}>2.8.2</Text>
+              <Text style={[styles.aboutValue, { color: colors.text }]}>2.8.3</Text>
             </View>
 
             <View style={styles.aboutRow}>
