eff95ff0db31f4bd4787f2a7dea172a54a37cd87
David Young <sporty-iron883@protonmail.com>
2025-12-14 14:46:04 -0500

    Release 2.13.10

--- src/mobile/Settings.jsx
+++ src/mobile/Settings.jsx
@@ -485,7 +485,7 @@ export default function Settings({ appConfig, setDismiss }) {
 
             <View style={styles.aboutRow}>
               <Text style={[styles.aboutLabel, { color: colors.textMuted }]}>Version</Text>
-              <Text style={[styles.aboutValue, { color: colors.text }]}>2.13.4</Text>
+              <Text style={[styles.aboutValue, { color: colors.text }]}>2.13.10</Text>
             </View>
 
             <View style={styles.aboutRow}>
